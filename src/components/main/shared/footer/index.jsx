2988aea4809c33b9c9d5b77cfc251ec65c1beada
tofayel <tofayeltuhin143@gmail.com>
2025-02-27 17:45:36 +0600

    footer

--- src/components/main/shared/footer/index.jsx
+++ src/components/main/shared/footer/index.jsx
@@ -1,16 +1,21 @@
 import logo from "@/assets/Logo.png";
-import facebook from '@/assets/icons/facebook-icon.svg';
-import linkedin from '@/assets/icons/linkedin-icon.svg';
-import youtube from '@/assets/icons/youtube-icon.svg';
-import footerUpper from '@/assets/icons/footer-upper.svg';
-import footerLower from '@/assets/icons/footer-lower.svg';
+import facebook from "@/assets/icons/facebook-icon.svg";
+import linkedin from "@/assets/icons/linkedin-icon.svg";
+import youtube from "@/assets/icons/youtube-icon.svg";
+import footerUpper from "@/assets/icons/footer-upper.svg";
+import footerLower from "@/assets/icons/footer-lower.svg";
+import Locationicon from "@/assets/icons/location-icon";
 
 const Footer = () => {
   return (
     <div className=" bg-[#080D1E] pt-16 pb-8 relative overflow-hidden">
       {/* Pseudo-elements for the shadow */}
-      <div className="absolute top-0 left-0 z-[4]"><img src={footerUpper} /></div>
-      <div className="absolute bottom-0 right-0 z-[4] rotate-180"><img src={footerLower} /></div>
+      <div className="absolute top-0 left-0 z-[4]">
+        <img src={footerUpper} />
+      </div>
+      <div className="absolute bottom-0 right-0 z-[4] rotate-180">
+        <img src={footerLower} />
+      </div>
 
       <div className="mx-auto container relative z-10">
         <div className="flex flex-col justify-between sm:px-[18px] md:flex-row md:px-10">
@@ -54,8 +59,10 @@ const Footer = () => {
               <ul>
                 {[
                   { name: "Home", href: "/" },
-                  { name: "News", href: "/our-tutors" },
-                  { name: "Contact", href: "/become-a-tutor" },
+                  { name: "Browser", href: "" },
+                  { name: "Sell", href: "" },
+                  { name: "About", href: "/" },
+                  { name: "Contact Us", href: "" },
                 ].map((link, index) => (
                   <li key={index} className="mt-[15px]">
                     <a
@@ -76,9 +83,10 @@ const Footer = () => {
               </p>
               <ul>
                 {[
-                  { name: "Home", href: "/" },
-                  { name: "News", href: "/our-tutors" },
-                  { name: "Contact", href: "/become-a-tutor" },
+                  { name: "Pricing", href: "/" },
+                  { name: "FAQs", href: "/" },
+                  { name: "How it works", href: "/" },
+                  { name: "Blog", href: "/" },
                 ].map((link, index) => (
                   <li key={index} className="mt-[15px]">
                     <a
@@ -97,22 +105,15 @@ const Footer = () => {
               <p className="text-[18px] font-medium text-primary leading-normal">
                 Office Location{" "}
               </p>
-              <ul>
-                {[
-                  { name: "Home", href: "/" },
-                  { name: "News", href: "/our-tutors" },
-                  { name: "Contact", href: "/become-a-tutor" },
-                ].map((link, index) => (
-                  <li key={index} className="mt-[15px]">
-                    <a
-                      className="text-[15px] font-normal hover:font-semibold hover:text-white/80"
-                      href={link.href}
-                    >
-                      {link.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
+              <address>
+                Emily Hattson 940 Goldendale Dr, Wasilla, Alaska 99654, USA
+              </address>
+              <div className="mt-4 flex gap-2 items-center">
+                <Locationicon />
+                <p className="text-[16px] font-normal text-white/80">
+                  See on Map
+                </p>
+              </div>
             </div>
           </div>
         </div>
