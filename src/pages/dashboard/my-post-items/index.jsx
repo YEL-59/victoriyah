9915515e560bf44ffc4a23905698b3ca57e177ac
OmarRabbi <omarfaruquerabbi0@gmail.com>
2025-02-27 15:55:44 +0600

    dashboard layout created

--- src/pages/dashboard/my-post-items/index.jsx
+++ src/pages/dashboard/my-post-items/index.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function MyPostItems() {
   return (
-    <div>MyPostItems</div>
+    <div>
+      <h3 className='text-3xl leading-[132%] font-semibold tracking-[-0.48px] text-[#315215]'>My Post Items</h3>
+    </div>
   )
 }
 
