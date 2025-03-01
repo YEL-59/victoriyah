c2f439b3bd92d42207bc87241bd41db253106ed8
tofayel <tofayeltuhin143@gmail.com>
2025-03-01 14:59:08 +0600

    signin auth

--- src/pages/main/paymentDetails/index.jsx
+++ src/pages/main/paymentDetails/index.jsx
@@ -86,12 +86,12 @@ const PaymentDetails = () => {
               render={({ field }) => (
                 <FormItem>
                   <FormLabel className="text-[18px] font-normal leading-[164%]">
-                    Item Name *
+                    Card Number
                   </FormLabel>
                   <FormControl>
                     <Input
                       type="text"
-                      placeholder="Enter your name"
+                      placeholder="12345687"
                       {...field}
                       className="w-full   bg-white"
                     />
@@ -100,118 +100,62 @@ const PaymentDetails = () => {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="text"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-[18px] font-normal leading-[164%]">
-                    Category *
-                  </FormLabel>
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
+            <div className="flex justify-between gap-5">
+              <div className="w-full">
+                {" "}
+                <FormField
+                  control={form.control}
+                  name="text"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-[18px] font-normal leading-[164%]">
+                        Expiry Date
+                      </FormLabel>
                       <FormControl>
-                        <SelectTrigger className="py-2 bg-white">
-                          <SelectValue placeholder="Select a verified email to display" />
-                        </SelectTrigger>
+                        <Input
+                          type="text"
+                          placeholder="12345687"
+                          {...field}
+                          className="w-full   bg-white"
+                        />
                       </FormControl>
-                      <SelectContent>
-                        <SelectItem value="m@example.com">
-                          m@example.com
-                        </SelectItem>
-                        <SelectItem value="m@google.com">
-                          m@google.com
-                        </SelectItem>
-                        <SelectItem value="m@support.com">
-                          m@support.com
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="text"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-[18px] font-normal leading-[164%]">
-                    Category *
-                  </FormLabel>
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="w-full">
+                {" "}
+                <FormField
+                  control={form.control}
+                  name="text"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-[18px] font-normal leading-[164%]">
+                        cvv
+                      </FormLabel>
                       <FormControl>
-                        <SelectTrigger className="py-2 bg-white">
-                          <SelectValue placeholder="Select a verified email to display" />
-                        </SelectTrigger>
+                        <Input
+                          type="text"
+                          placeholder="12345687"
+                          {...field}
+                          className="w-full   bg-white"
+                        />
                       </FormControl>
-                      <SelectContent>
-                        <SelectItem value="m@example.com">
-                          m@example.com
-                        </SelectItem>
-                        <SelectItem value="m@google.com">
-                          m@google.com
-                        </SelectItem>
-                        <SelectItem value="m@support.com">
-                          m@support.com
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+
             <FormField
               control={form.control}
               name="text"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel className="text-[18px] font-normal leading-[164%]">
-                    Category *
-                  </FormLabel>
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger className="py-2 bg-white">
-                          <SelectValue placeholder="Select a verified email to display" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="m@example.com">
-                          m@example.com
-                        </SelectItem>
-                        <SelectItem value="m@google.com">
-                          m@google.com
-                        </SelectItem>
-                        <SelectItem value="m@support.com">
-                          m@support.com
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="text"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-[18px] font-normal leading-[164%]">
-                    Location *
+                    Name on Card
                   </FormLabel>
                   <FormControl>
                     <Input
@@ -260,6 +204,23 @@ const PaymentDetails = () => {
                 </FormItem>
               )}
             />
+
+            <h1 className="text-[24px] font-semibold leading-[132%] tracking-[-0.48px] mt-5 mb-5 text-foreground">
+              Review & Confirm
+            </h1>
+
+            <div className="flex justify-between gap-5 border-b-2 pb-5">
+              {" "}
+              <div className="w-full">
+                <p className="text-lg text-gray-600">Payment Method</p>
+                <p className="text-lg text-gray-600">Paypal</p>
+              </div>
+              <div className="w-full">
+                <p className="text-lg text-gray-600">Card Number</p>
+                <p className="text-lg text-gray-600">12345687</p>
+              </div>
+            </div>
+
             <div className="flex justify-between">
               <div className="w-full">
                 <button
